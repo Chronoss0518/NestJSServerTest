bc4441a8a17c8cc137de8707abd8195f59a65b91
Chronoss0518 <sinyusuke5000@gmail.com>
2023-12-29 16:15:20 +0900

    軽くAuthとuserを作成

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller,Get } from '@nestjs/common';
+import { StringifyOptions } from 'querystring';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+
+    constructor(private readonly userService: UserService) {}
+
+    @Get("id")
+    getId():string
+    {
+        return this.userService.getId();
+    }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+    
+    getId() :number
+    {
+        return 0;
+    }
+
+    getName():string
+    {
+        return "";
+    }
+
+}
