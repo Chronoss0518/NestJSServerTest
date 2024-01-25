ca2a3fc36804b30d154543dad26d383bfdbe1fec
Chronoss0518 <sinyusuke5000@gmail.com>
2024-01-25 20:01:40 +0900

    テスト用のポート番号を修正

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = Number(process.env.PORT) || 8080;
+  const port = Number(process.env.PORT) || 8180;
   await app.listen(port, '0.0.0.0');
 }
 
